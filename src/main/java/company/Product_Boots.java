8179cfa97fed1d3160b90736ef571cd431770c70
IrekBaranski <irekbaranski@gmail.com>
2018-09-29 22:33:03 +0200

    update

--- src/main/java/company/Product_Boots.java
+++ src/main/java/company/Product_Boots.java
@@ -1,4 +1,18 @@
 package company;
 
-public class Product_boots {
+public class Product_Boots extends Product{
+    // Klasa Boots – dziedzicząca po Product:
+    //
+    //size (liczba całkowita)
+    //isNaturalSkin (boolean)
+    int size;
+    Boolean isNaturalSkin;
+
+    public Product_Boots (int id, String productName, String color, double weight, double price, int productCount, int size, boolean isNaturalSkin){
+        super (id, productName, color, weight, price, productCount);
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+    //kreator Product (int id, String productName, String color, double weight, double price, int productCount)
 }
--- src/main/java/company/Product_Cloth.java
+++ src/main/java/company/Product_Cloth.java
@@ -1,4 +1,23 @@
 package company;
 
-public class Product_Clothes {
+// klasa rozszerza product o
+// Ssize (String)
+//material (String)
+
+public class Product_Cloth extends Product{
+    String size;
+    String material;
+
+    public Product_Cloth (int id, String productName, String color, double weight, double price, int productCount, String size, String material) {
+        super (id, productName, color, weight, price, productCount);
+        this.size = size;
+        this.material = material;
+
+        //kreator Product (int id, String productName, String color, double weight, double price, int productCount)
+
+    }
+
+
+
+
 }
